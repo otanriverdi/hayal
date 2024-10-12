d36b34fd8ed3433f2e779dc14896dea5bd6fefcd
Ozgur <ozgur.tanriverdi94@gmail.com>
2024-10-12 12:56:35 +0300

    improve docs

--- ecs/system.go
+++ ecs/system.go
@@ -1,9 +1,14 @@
 package ecs
 
 type SystemCtx interface {
+	// Spawn initializes a new entity with the passed in component.
 	Spawn(cmp any) (entity, error)
+	// Destroy de-initializes the passed in entity.
 	Destroy(entity entity) error
+	// AddComponent adds the passed in component to the entity.
 	AddComponent(entity entity, cmp any) error
+	// RemoveComponent removes the passed in component from the entity.
 	RemoveComponent(entity entity, cmp any) error
+	// Query returns an iterator of all entities that includes the passed in components.
 	Query(cmps ...any) (func(yield func(QueryResult) bool), error)
 }
--- hayal.go
+++ hayal.go
@@ -1,3 +1,55 @@
+// Package hayal provides an experimental ecs based game engine. 
+//
+//
+// To the Game struct, you can assign systems to be executed in various steps of the game engine, that would initialize
+// it's own entities and update logic:
+//
+//	game := hayal.New()
+//	game.AddSystem(hayal.GameLoopStepUpdate, system)
+//	game.Run()
+//
+// The system functions have access to a gameCtx that can be used to interface with the game world. 
+//
+//  func System(ctx *gameCtx) {
+//    e, err := ctx.Spawn(transform{x: 5, y: 10})
+//    if err != nil {
+//      return err
+//    }
+//    
+//    err = ctx.Destroy(e)
+//    if err != nil {
+//      return err
+//    }
+//
+//    err = ctx.AddComponent(e, 5)
+//    if err != nil {
+//      return err
+//    }
+//
+//    err = ctx.RemoveComponent(e, 5)
+//    if err != nil {
+//      return err
+//    }
+//
+//    iter, err := ecs.Query(transform{})
+//    if err != nil {
+//      return err
+//    }
+//    for entity := range iter {
+//      cmp, err := GetComponent[transform](&entity)
+//      if err != nil {
+//        return err
+//      }
+//
+//      err = SetComponent(&entity, transform{x: 20, y: 23})
+//      if err != nil {
+//        return err
+//      }
+//    }
+//
+//    ctx.Exit()
+//  }
+//
 package hayal
 
 import (
@@ -15,42 +67,53 @@ func (ctx *gameCtx) Exit() {
 	close(ctx.exit)
 }
 
-type GameLoopStep = uint
+type gameLoopStep = uint
 
 const (
-	GameLoopStepInit GameLoopStep = iota
+	// GameLoopStepInit runs once on game start.
+	GameLoopStepInit gameLoopStep = iota
+	// GameLoopStepPreUpdate runs first on every tick. This is where we update resources that would be used in the
+	// update stage.
 	GameLoopStepPreUpdate
+	// GameLoopStateUpdate runs on every tick. This is where we update game play systems.
 	GameLoopStateUpdate
+	// GameLoopStateDraw runs on every tick after Update. This is where we render and apply component updates.
 	GameLoopStateDraw
+	// GameLoopStateDeinit runs once before exit. Use this for cleanup.
 	GameLoopStateDeinit
 )
 
-type systemCtx interface {
+type SystemCtx interface {
 	ecs.SystemCtx
+	// Exit signals the game to run cleanup and exit gracefully.
 	Exit()
 }
 
-type System = func(ctx systemCtx) error
+type System = func(ctx SystemCtx) error
 
-type game struct {
+type Game struct {
 	ctx *gameCtx
 	// len of first dimension matches step count
 	schedule [5][]System
 }
 
-func New() game {
+// New initializes a new game.
+func New() Game {
 	e := ecs.New()
-	return game{ctx: &gameCtx{ECS: e}}
+	return Game{ctx: &gameCtx{ECS: e}}
 }
 
-func (g *game) AddSystem(step GameLoopStep, system System) {
+// AddSystem adds a system to the schedule to be executed in various steps of the game loop. Check GameLoopStep*
+// constants for various steps and when they run.
+func (g *Game) AddSystem(step gameLoopStep, system System) {
 	if g.schedule[step] == nil {
 		g.schedule[step] = make([]System, 0)
 	}
 	g.schedule[step] = append(g.schedule[step], system)
 }
 
-func (g *game) Run() {
+// Run starts the schedule and the execution of the game.
+func (g *Game) Run() {
 	g.executeStep(GameLoopStepInit)
 	for {
 		select {
@@ -65,7 +128,7 @@ func (g *game) Run() {
 	}
 }
 
-func (g *game) executeStep(step GameLoopStep) {
+func (g *Game) executeStep(step gameLoopStep) {
 	var wg sync.WaitGroup
 	for _, sys := range g.schedule[GameLoopStepInit] {
 		wg.Add(1)
@@ -80,10 +143,12 @@ func (g *game) executeStep(step GameLoopStep) {
 	wg.Wait()
 }
 
+// GetComponent extracts a copy of component data from the passed in query result.
 func GetComponent[C any](qr *ecs.QueryResult) (C, error) {
 	return ecs.GetComponent[C](qr)
 }
 
+// SetComponent sets the component data to be the passed in component.
 func SetComponent(qr *ecs.QueryResult, cmp any) error {
 	return ecs.SetComponent(qr, cmp)
 }
